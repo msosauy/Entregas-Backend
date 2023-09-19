d168a05db9b8ee66929ea94afc80f011f1fe5a59
msosa <uy.msosa@gmail.com>
2023-09-19 19:37:50 -0300

    inicio de estilos en home.handlebars

--- src/public/js/home.js
+++ src/public/js/home.js
@@ -3,16 +3,25 @@ const socket = window.io();
 socket.on("realTimeProducts", (products) => {
   console.log(products);
   const list = document.getElementById("list");
+  const pagination = document.getElementById("pagination");
 
   while (list.firstChild) {
     list.removeChild(list.firstChild);
   }
+  
+  while (pagination.firstChild) {
+    pagination.removeChild(pagination.firstChild);
+  }
 
   let productList = "";
+  let paginationList = "";
 
   products.docs.forEach((el) => {
     productList = productList + `<li>${el.title} - U$S ${el.price}</li>`;
   });
 
+  paginationList = `<a href=${products.}>${products.totalPages}</a>`
+
   list.innerHTML = productList;
+  pagination.innerHTML = paginationList;
 });
