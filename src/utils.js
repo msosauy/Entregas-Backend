d01e766413df7d37d479a459630361a2f090c95f
msosa <uy.msosa@gmail.com>
2023-10-01 20:44:29 -0300

    desafio clase 21

--- src/utils.js
+++ src/utils.js
@@ -1,32 +1,9 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import bcrypt from "bcrypt";
-import jsonwt from "jsonwebtoken";
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = dirname(__fileName);
-const PRIVATE_KEY_token = "tokenKey098!lp";
-
-export const generateToken = (user) => {
-  const token = jsonwt.sign({user}, PRIVATE_KEY_token, {expiresIn: "24h"})
-  return token
-}
-
-export const authToken = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if(!authHeader) {
-    return res.status(401).send({status: "error", error: "No autenticado"})
-  }
-
-  const token = authHeader.split(" ")[1];
-  jsonwt.verify(token, PRIVATE_KEY_token, (error, credentials) => {
-    if (error) {
-      return res.status(403).send({status: "error", error: "No autorizado"})
-    }
-    req.user = credentials.user;
-    next();
-  })
-}
 
 export const createHash = (password) =>
   bcrypt.hashSync(password, bcrypt.genSaltSync(10));
