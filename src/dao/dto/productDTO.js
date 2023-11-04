ab582648862329d10b38600625a8bf7bc6818376
msosauy <uy.msosa@gmail.com>
2023-11-03 22:25:31 -0300

    drto

---
+++ src/dao/dto/productDTO.js
@@ -0,0 +1,13 @@
+export default class ProductDTO {
+    constructor(product) {
+      this.title = product.title;
+      this.description = product.description;
+      this.price = product.price;
+      this.stock = product.stock;
+      this.status = product.status;
+      this.category = product.category;
+      this.code = product.code;
+      this.thumbnails = product.thumbnails;
+      this.active = true;
+    }
+  }
